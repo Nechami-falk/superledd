73515bc4b36e4c6683204cd856eda06de69166a9
nechami falk <falknechami@gmail.com>
2022-12-27 10:22:22 +0200

    fix to deploy

--- src/services/fetchApiCompany.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import apiUrl  from '../config.json';
-
-async function fetchCompanies(){
-    const {data} = await axios.get(`${apiUrl.apiUrl}/companies`);
-    return data
-}
-
-export default fetchCompanies;
