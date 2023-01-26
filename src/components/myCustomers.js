d82007d12838574f48b1015f48cd3e1a6f14eec1
nechami falk <falknechami@gmail.com>
2023-01-26 18:09:04 +0200

    add search to customer

--- src/components/myCustomers.js
+++ src/components/myCustomers.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { CustomerService } from '../services/customerService'
+import { useForm } from 'react-hook-form';
 
 function MyCustomers() {
 
+  const { register, handleSubmit } = useForm(); 
 const navigate = useNavigate();
 const [customers, setCustomers] =  useState();
+
 useEffect(() => {
     getCustomerData();
 }, []);
@@ -33,8 +36,32 @@ const onDeleteCustomer = async (customerId) =>{
   getCustomerData();
 }
 
+const onDataSubmit = async(e) => {
+  console.log(e.target.value);
+  const data = e.target.value;
+  try{
+    const search = await CustomerService.searchCustomer(data);
+    console.log(search.data);
+    setCustomers(search.data);
+    if (!search) {
+      console.log('not valid');  
+    }
+  }
+  catch(ex){
+    console.log(ex);
+  }
+}
+
   return (
     <>
+    <div className='container'>
+      <div className='row col-lg-4 m-3'>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onKeyUp={(e) => {onDataSubmit(e)}} {...register("search")}/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      </div>
+    </div>
     <div className="container">
       <div className="container">
         <table className="table table-striped table-hover">
